510b66d5c00a7ef2ba850832b58c43a0c36d04e8
MinaYasser100 <minayasser43@gmail.com>
2024-07-30 01:08:31 +0300

    do dio post method

--- lib/core/api/dio_api.dart
+++ lib/core/api/dio_api.dart
@@ -1,1 +1,26 @@
-class DioApi {}
+import 'package:dio/dio.dart';
+import 'package:shop_iti_app/core/api/dio_end_point.dart';
+
+class DioApi {
+  static final Dio _dio = Dio(
+    BaseOptions(
+      baseUrl: DioEndPoint.dioUrl,
+      headers: {
+        'lang': 'en',
+        'Content-Type': 'application/json',
+      },
+    ),
+  );
+
+  static Future<Response> postData(
+      {required String endPoint,
+      Map<String, dynamic>? body,
+      Map<String, dynamic>? queryParameters}) async {
+    final response = await _dio.post(
+      endPoint,
+      data: body,
+      queryParameters: queryParameters,
+    );
+    return response;
+  }
+}
