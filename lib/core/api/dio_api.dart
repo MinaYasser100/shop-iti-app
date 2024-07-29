a1336a223fd7ba8c065185449267d4334ec503b1
MinaYasser100 <minayasser43@gmail.com>
2024-07-30 01:28:25 +0300

    finish dio api method

--- lib/core/api/dio_api.dart
+++ lib/core/api/dio_api.dart
@@ -2,24 +2,67 @@ import 'package:dio/dio.dart';
 import 'package:shop_iti_app/core/api/dio_end_point.dart';
 
 class DioApi {
-  static final Dio _dio = Dio(
-    BaseOptions(
-      baseUrl: DioEndPoint.dioUrl,
-      headers: {
-        'lang': 'en',
-        'Content-Type': 'application/json',
-      },
-    ),
-  );
+  static Dio? _dio;
+  static initDio() {
+    _dio = Dio(
+      BaseOptions(
+        baseUrl: DioEndPoint.dioUrl,
+        receiveDataWhenStatusError: true,
+      ),
+    );
+  }
 
-  static Future<Response> postData(
-      {required String endPoint,
-      Map<String, dynamic>? body,
-      Map<String, dynamic>? queryParameters}) async {
-    final response = await _dio.post(
+  static Future<Response> postData({
+    required String endPoint,
+    Map<String, dynamic>? body,
+    Map<String, dynamic>? queryParameters,
+    String? token,
+  }) async {
+    _dio!.options.headers = {
+      'lang': 'en',
+      'Content-Type': 'application/json',
+      'Authorization': token ?? '',
+    };
+    final response = await _dio!.post(
       endPoint,
       data: body,
       queryParameters: queryParameters,
+    );
+    return response;
+  }
+
+  static Future<Response> getData({
+    required String endPoint,
+    String? token,
+    Map<String, dynamic>? queryParameters,
+  }) async {
+    _dio!.options.headers = {
+      'lang': 'en',
+      'Content-Type': 'application/json',
+      'Authorization': token ?? '',
+    };
+    final response = await _dio!.get(
+      endPoint,
+      queryParameters: queryParameters,
+    );
+    return response;
+  }
+
+  static Future<Response> putData({
+    required String endPoint,
+    String? token,
+    Map<String, dynamic>? queryParameters,
+    Map<String, dynamic>? body,
+  }) async {
+    _dio!.options.headers = {
+      'lang': 'en',
+      'Content-Type': 'application/json',
+      'Authorization': token ?? '',
+    };
+    final response = await _dio!.put(
+      endPoint,
+      queryParameters: queryParameters,
+      data: body,
     );
     return response;
   }
