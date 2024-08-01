2ada145e8433bb7513e5b9cf0bb91d0710e9b49d
MinaYasser100 <minayasser43@gmail.com>
2024-08-02 00:36:39 +0300

    do the favorite model

--- lib/core/constant/constant.dart
+++ lib/core/constant/constant.dart
@@ -3,4 +3,6 @@ import 'package:flutter/material.dart';
 class ConstantComponents {
   static const Color firstColor = Color.fromARGB(244, 90, 130, 115);
   static const Color secondColor = Color.fromARGB(255, 244, 242, 236);
+  static const token =
+      'rRNTIum9ChO5jfsoP2bXcXLLXlYnu4GMAyoYWhBhMiZrw597WN1E0GqhLWp4ZxaoTTv4mW';
 }
---
+++ lib/features/layout/presentation/views/widgets/shop/data/model/favorite_model/datum.dart
@@ -0,0 +1,20 @@
+import 'fav_product.dart';
+
+class Datum {
+  int? id;
+  FavProduct? product;
+
+  Datum({this.id, this.product});
+
+  factory Datum.fromJson(Map<String, dynamic> json) => Datum(
+        id: json['id'] as int?,
+        product: json['product'] == null
+            ? null
+            : FavProduct.fromJson(json['product'] as Map<String, dynamic>),
+      );
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'product': product?.toJson(),
+      };
+}
