17de2a82a2e6a4c66a6ef71fe15498f1bf7ea7a4
MinaYasser100 <minayasser43@gmail.com>
2024-08-05 17:02:18 +0300

    active on tap for items search

--- lib/features/search/presentation/views/widgets/search_result_list_view.dart
+++ lib/features/search/presentation/views/widgets/search_result_list_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:shop_iti_app/core/pages/get_pages.dart';
 import 'package:shop_iti_app/features/layout/data/model/proudect_model/product.dart';
 
 import 'search_item_product_widget.dart';
@@ -14,8 +16,16 @@ class SearchResultListView extends StatelessWidget {
     return ListView.separated(
       shrinkWrap: true,
       physics: const NeverScrollableScrollPhysics(),
-      itemBuilder: (context, index) => SearchItemProductWidget(
-        productItemModel: resultProducts[index],
+      itemBuilder: (context, index) => GestureDetector(
+        onTap: () {
+          Get.toNamed(
+            GetPages.kProductDetails,
+            arguments: resultProducts[index],
+          );
+        },
+        child: SearchItemProductWidget(
+          productItemModel: resultProducts[index],
+        ),
       ),
       separatorBuilder: (context, index) => const SizedBox(
         height: 10,
