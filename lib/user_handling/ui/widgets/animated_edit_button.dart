977c3e783641774d0203b4bae493c7e48b906181
MinaYasser100 <minayasser43@gmail.com>
2024-08-08 01:21:55 +0300

    change color

--- lib/user_handling/ui/widgets/animated_edit_button.dart
+++ lib/user_handling/ui/widgets/animated_edit_button.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:shop_iti_app/core/constant/constant.dart';
 import 'package:shop_iti_app/user_handling/ui/widgets/submit_form_button.dart';
@@ -18,40 +17,42 @@ class AnimatedEditButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => LayoutBuilder(
-    builder: (context, c) => Stack(
-      children: [
-        IgnorePointer(
-          ignoring: !editClicked,
-          child: Align(
-            alignment: Alignment.centerLeft,
-            child: SubmitFormButton(
-              title: editClicked ? "Cancel" : "Edit",
-              bgColor: editClicked ? Colors.redAccent : ConstantComponents.firstColor,
-              width: editClicked ? c.maxWidth * 0.325 : c.maxWidth,
-              onTap: (){
-                if(editClicked){
-                  onCancel();
-                }
-              },
+        builder: (context, c) => Stack(
+          children: [
+            IgnorePointer(
+              ignoring: !editClicked,
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: SubmitFormButton(
+                  title: editClicked ? "Cancel" : "Edit",
+                  bgColor: editClicked
+                      ? Colors.redAccent
+                      : ConstantComponents.firstColor,
+                  width: editClicked ? c.maxWidth * 0.325 : c.maxWidth,
+                  onTap: () {
+                    if (editClicked) {
+                      onCancel();
+                    }
+                  },
+                ),
+              ),
             ),
-          ),
+            Align(
+              alignment: Alignment.centerRight,
+              child: SubmitFormButton(
+                title: editClicked ? "Save" : "Edit",
+                bgColor: ConstantComponents.firstColor,
+                width: editClicked ? c.maxWidth * 0.625 : c.maxWidth,
+                onTap: () {
+                  if (editClicked) {
+                    onSave();
+                  } else {
+                    onEdit();
+                  }
+                },
+              ),
+            ),
+          ],
         ),
-        Align(
-          alignment: Alignment.centerRight,
-          child: SubmitFormButton(
-            title: editClicked ? "Save" : "Edit",
-            bgColor: editClicked ? Colors.blueAccent : ConstantComponents.firstColor,
-            width: editClicked ? c.maxWidth * 0.625 : c.maxWidth,
-            onTap: (){
-              if(editClicked){
-                onSave();
-              }else{
-                onEdit();
-              }
-            },
-          ),
-        ),
-      ],
-    ),
-  );
+      );
 }
